0d01ab4101e498bb5b165650ef85d04013e2d403
Jason Liang <jaisan.liang@gmail.com>
2014-06-12 15:59:19 -0400

    First implementation of K-means

--- KMeans.py
+++ KMeans.py
@@ -1,44 +1,45 @@
 """
 Implementation of K-means algorithm
-Assumes user inputs data in the form of a list of feature vectors
 """
 
 import pandas
-import csv
-import re
 import random
+
+k = 2
 
 def distance(vector1,vector2):
     return sum((vector1[i]-vector2[i])^2 for i in range(len(vector1)))
 
-def findClosest(vector,meansList):
-    closestDist = distance(vector, meansList[0])
-    closestMean = meansList[0]
-    for i in len(meansList):
-        mean = meansList[i]
-        if distance(vector,mean) < closestDist:
-            closestDist = distance(vector,mean)
-            closestMean = i
+def findClosest(vector,centers):
+    closestDist = distance(vector, centers[0])
+    closestCenter = 0
+    for i in len(centers):
+        center = centers[i]
+        if distance(vector,center) < closestDist:
+            closestDist = distance(vector,center)
+            closestCenter = i
+    return closestCenter
 
-inputFilePath = input('Path to input file: ')
-vectorList = DataFrame.from_csv(inputFilePath)
-#get the number of features in each vector
-numFeatures = len(vectorList[0])
+def allConverged(centers,lastCenters):
+    for i in range(len(centers)):
+        if not Auxiliary.converged(centers[i],lastCenters[i]):
+            return False
+    return True
 
-#set K
-k = 2
-
-# randomly choose k starting vectors, pop them so we don't get duplicates
-meansList = []
-for i in range(k):
-    newStart = vectorList.pop(random.choice(vectorList))
-    startVectors.append(newStart)
-for i in range(k):
-    vectorList.append(startVectors[i])
-
-# repeat algorithm until convergence
-for i in range(10):
-    closestAssignments = dict([] for j in range(k))
-    for vector in vectorList:
-        index = findClosest(vector,meansList)
-        closestAssignments[index].append
+def getClusterCenters(vectors):
+    numFeatures = len(vectors.loc[0])
+    # randomly choose k starting vectors
+    centers = pandas.Series([])
+    for i in range(k):
+        newStart = vectors.loc[random.randint(1,len(vectors))]
+        centers.append(pandas.Series([newStart]))
+    lastCenters = centers
+    # repeat algorithm until convergence
+    while not allConverged(centers,lastCenters):
+        lastCenters = centers
+        for i in len(vectors):
+            closestCenter = findClosest(vector,centers)
+            vectors['Closest Center'] = closestCenter
+        grouped = vectors.groupby('Closest Center')
+        centers = grouped.mean()
+    return centers
