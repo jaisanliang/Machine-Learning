2225271c43409c4063efc978df7cc939903dac5b
jaisanliang <jaisan.liang@gmail.com>
2014-08-13 08:33:20 -0400

    Implemented regularize

--- Auxiliary.py
+++ Auxiliary.py
@@ -1,27 +1,40 @@
 '''
-Module that contains auxiliary functions,
+Module that contains auxiliary functions for machine learning,
 including calculators for precision and recall
 '''
 
-import pandas
+import numpy as np
 
-def normalize(vectors):
-    means = vectors.mean()
-    stds = vectors.std()
-    for i in range(len(vectors)):
-        for j in range(len(vectors.loc[i])):
-            # what is std=0?
-            vectors.loc[i][j] = (vectors.loc[i][j]-means[j])/stds[j]
-    return vectors
+def regularize(vectors):
+    '''
+    Normalize the vectors by expressing each vector 
+    in standard normal form (vectors is numpy array)
+    '''
+    normVectors=np.float_(vectors)
+    # get avg and std of each feature
+    means=np.average(normVectors,axis=0)
+    stds=np.std(normVectors,axis=0)
+    for i in range(len(normVectors)):
+        for j in range(len(normVectors[i])):
+            # take care of the case where std=0
+            if stds[j]==0:
+                normVectors[i][j]=0.0
+            else:
+                normVectors[i][j]=(1.0*normVectors[i][j]-means[j])/stds[j]
+    return normVectors
         
-def converged(weights,prevWeights):
-    epsilon = 0.0001
+def hasConverged(weights,prevWeights):
+    '''
+    Determine if the learned weights for a feature have converged
+    '''
+    numFeatures=len(weights)
+    epsilon=0.0001
     for i in range(numFeatures):
-        if (weights[i]-prevWeights[i]) > epsilon:
+        if (weights[i]-prevWeights[i])>epsilon:
             return False
     return True
 
-def precision(actualVsPredict):
+#def precision(actualVsPredict):
     
 
-def recall(actualVsPredict):
+#def recall(actualVsPredict):
