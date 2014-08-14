f999a561112d5bfebe8786add33ee2aa41d94ffd
Jason Liang <jaisan.liang@gmail.com>
2014-08-14 11:51:51 -0400

    Completed LogReg pending tests
    Could be combined with LinReg

--- LogReg.py
+++ LogReg.py
@@ -4,31 +4,48 @@ a logistic model to find feature weights
 '''
 
 import math
-import pandas
+import numpy as np
+import pandas as pd
 import Auxiliary
 
-numFeatures = 0
-learnRate = 0.001
+NUMFEATURES = 3
+LEARNRATE = 0.001
 
-def calculateWeights(vectors):
-    '''
-    vector is DataFrame with correct value at end
-    '''
-    numFeatures = len(vectors.loc[0])-1
-    weights = pandas.Series([0 for i in range(numFeatures)])
-    prevWeights = weights
-    while not Auxiliary.converged(weights,prevWeights):
-        prevWeights = weights
-        weights = descentStep(vectors,weights)
-    return weights
+def vector_cost(vector,weights):
+    """Calculates difference between actual and predicted (given feature vector) value"""
+    predictedVal = sum(vector[i]*weights[i] for i in range(NUMFEATURES))
+    return vector[NUMFEATURES]-1.0/(1+math.e**(-predictedVal))
 
-def vectorCost(vector,weights):
-    predictedVal = sum(vector[i]*weights[i] for i in range(numFeatures))
-    return vector[numFeatures]-1.0/(1+math.e**(-predictedVal))
-
-def descentStep(vectors,weights):
+def batchDescentStep(vectors,weights):
+    """Calculates new feature weights after one batch descent step"""
     newWeights = weights
-    for i in range(numFeatures):
-        derivative = sum(vectorCost(vectors.loc[j],weights)*vectors.loc[j][i] for j in range(len(vectors)))
-        newWeights[i] += learnRate*derivative
+    for i in range(NUMFEATURES):
+        derivative = sum(vector_cost(vectors[j],weights)*vectors[j][i] for j in range(len(vectors)))
+        newWeights[i] += LEARNRATE*derivative
     return newWeights
+
+def stochasticDescentStep(vector,weights):
+    """Calculates new feature weights after one stochastic descent step"""
+    newWeights = weights
+    for i in range(NUMFEATURES):
+        derivative = vector_cost(vector,weights)*vector[i]
+        newWeights[i] += LEARNRATE*derivative
+    return newWeights
+
+def calculate_weights(vectors,batch=True):
+    """
+    Main method for calculating weights of features
+    Vector is feature vector with correct classification appended
+    """
+    NUMFEATURES = len(vectors[0])-1
+    weights = np.array([0 for i in range(NUMFEATURES)])
+    stochasticIndex = 0
+    while True:
+        prevWeights = weights
+        if batch:
+            weights = batchDescentStep(vectors,weights)
+        else:
+            weights = stochasticDescentStep(vectors[stochasticIndex],weights)
+            stochasticIndex = (stochasticIndex+1)%len(vectors)
+        if Auxiliary.converged(weights,prevWeights):
+            return weights
