a9274aa091f6aafb1af51eb65f97c7798e405295
Jason Liang <jaisan.liang@gmail.com>
2014-08-15 16:37:07 -0400

    Need to implement propagation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -4,40 +4,58 @@ Module for neural network
 
 import math
 import random
-import pandas
+import numpy as np
+import pandas as pd
 import Auxiliary
 
-# each element of the array is
-layers = []
-numLayers = len(layers)
-numFeatures = 0
-initRange = 0.01
-learnRate = 0.001
+# each element of the layers array is the number of units in that network
+LAYERS = []
+NUMLAYERS = len(layers)
+NUMFEATURES = 0
+INITRANGE = 0.01
+LEARNRATE = 0.001
 
-def calculateWeights(vectors):
-    '''
-    vector is DataFrame with correct value at end
-    '''
-    weights = pandas.Series([[random.uniform(-initRange,initRange) for i in range(layers[j])] for j in range(numLayers)])
-    prevWeights = weights
-    while not Auxiliary.converged(weights,prevWeights):
+def vector_cost(vector,weights):
+    """Calculates difference between actual and predicted (given feature vector) value"""
+    predictedVal = sum(vector[i]*weights[i] for i in range(NUMFEATURES))
+    return vector[NUMFEATURES]-1.0/(1+math.e**(-predictedVal))
+
+def batchDescentStep(vectors,weights):
+    """Calculates new feature weights after one batch descent step"""
+    newWeights = weights
+    for i in range(NUMFEATURES):
+        derivative = sum(vector_cost(vectors[j],weights)*vectors[j][i] for j in range(len(vectors)))
+        newWeights[i] += LEARNRATE*derivative
+    return newWeights
+
+def stochasticDescentStep(vector,weights):
+    """Calculates new feature weights after one stochastic descent step"""
+    newWeights = weights
+    for i in range(NUMFEATURES):
+        derivative = vector_cost(vector,weights)*vector[i]
+        newWeights[i] += LEARNRATE*derivative
+    return newWeights
+
+def calculate_weights(vectors,batch=True):
+    """
+    Main method for calculating weights of features
+    Vector is feature vector with correct classification appended
+    """
+    NUMFEATURES = len(vectors[0])-1
+    weights = np.array([[random.uniform(-INITRANGE,INITRANGE) for i in range(LAYERS[j])] for j in range(NUMLAYERS)])
+    stochasticIndex = 0
+    while True:
         prevWeights = weights
-        weights = descentStep(vectors,weights)
-    return weights
+        if batch:
+            weights = batchDescentStep(vectors,weights)
+        else:
+            weights = stochasticDescentStep(vectors[stochasticIndex],weights)
+            stochasticIndex = (stochasticIndex+1)%len(vectors)
+        if Auxiliary.converged(weights,prevWeights):
+            return weights
 
 def forwardPropagate():
     
 
 def backPropagate():
     
-
-def vectorCost(vector,weights):
-    predictedVal = sum(vector[i]*weights[i] for i in range(numFeatures))
-    return vector[numFeatures]-1.0/(1+math.e**(-predictedVal))
-
-def descentStep(vectors,weights):
-    newWeights = weights
-    for i in range(numFeatures):
-        derivative = sum(vectorCost(vectors.loc[j],weights)*vectors.loc[j][i] for j in range(len(vectors)))
-        newWeights[i] += learnRate*derivative
-    return newWeights
