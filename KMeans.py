de1836835d81172f8a5f48c6ffe90781a0468608
jaisanliang <jaisan.liang@gmail.com>
2014-08-15 08:37:30 -0400

    Debegging KMeans

--- KMeans.py
+++ KMeans.py
@@ -10,18 +10,18 @@ import random
 # number of centers
 K = 2
 
-def distance(vector1,vector2):
+def distance2(vector1,vector2):
     """Calculates Cartesian distance between two feature vectors"""
-    return sum((vector1[i]-vector2[i])^2 for i in range(len(vector1)))^0.5
+    return sum((vector1[i]-vector2[i])**2 for i in range(len(vector1)))
 
 def find_closest(vector,centers):
     """Finds index of closest center to vector"""
-    closestDist = distance(vector, centers[0])
+    closestDist = distance2(vector, centers[0])
     closestCenter = 0
-    for i in len(centers):
+    for i in range(len(centers)):
         center = centers[i]
-        if distance(vector,center) < closestDist:
-            closestDist = distance(vector,center)
+        if distance2(vector,center) < closestDist:
+            closestDist = distance2(vector,center)
             closestCenter = i
     return closestCenter
 
@@ -36,10 +36,12 @@ def get_centers(vectors):
     """Main method for finding cluster centers"""
     numFeatures = len(vectors[0])
     # randomly choose k starting vectors
-    centers = np.array([])
+    centers = []
     # copy vectors before choosing centers so that centers aren't repeated
     newVectors = vectors
-    indexedVectors = pd.DataFrame(vectors)
+    print vectors[0][0]
+    indexedVectors = pd.DataFrame(vectors,columns=range(numFeatures))
+    print indexedVectors.loc[0]
     closestIndex = [0 for i in range(len(vectors))]
     for i in range(K):
         newStart = newVectors[random.randint(0,len(vectors)-1)]
@@ -48,12 +50,19 @@ def get_centers(vectors):
     # repeat algorithm until convergence
     while True:
         lastCenters = centers
-        for i in len(vectors):
-            closestCenter = findClosest(vector,centers)
+        for i in range(len(vectors)):
+            closestCenter = find_closest(vectors[i],centers)
             closestIndex[i] = closestCenter
         indexedVectors['Closest Center'] = closestIndex
         groupedVectors = indexedVectors.groupby('Closest Center')
-        centers = np.array(grouped.mean())
+        centers = np.array(groupedVectors.mean())
         if all_converged(centers,lastCenters):
             break
     return centers
+
+irisFile=open("iris.txt","r")
+irisArray=[]
+for line in irisFile:
+    irisArray.append([float(i) for i in line.split(",")[:-1]])
+print irisArray[:3]
+get_centers(np.array(irisArray))
