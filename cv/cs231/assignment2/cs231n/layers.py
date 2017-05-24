842681b32ab5b4fb84491b487f78a3c277356943
Jason Liang <liangjy@mit.edu>
2017-05-23 20:14:11 -0400

    cs231 homework 2 complete - without tutorials

--- cv/cs231/assignment2/cs231n/layers.py
+++ cv/cs231/assignment2/cs231n/layers.py
@@ -308,8 +308,8 @@ def dropout_forward(x, dropout_param):
         # TODO: Implement training phase forward pass for inverted dropout.   #
         # Store the dropout mask in the mask variable.                        #
         #######################################################################
-        mask = np.random.rand(*x.shape) < p
-        out = x*mask*(1/p)
+        mask = np.random.rand(*x.shape) > p
+        out = x*mask*(1/(1-p))
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -344,7 +344,7 @@ def dropout_backward(dout, cache):
         #######################################################################
         # TODO: Implement training phase backward pass for inverted dropout   #
         #######################################################################
-        dx = dout*mask/dropout_param['p']
+        dx = dout*mask/(1-dropout_param['p'])
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
