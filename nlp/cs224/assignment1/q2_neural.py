d7346991a0a7d4bb2a8f417534d0c9a8f41c6c38
Jason Liang <liangjy@mit.edu>
2017-06-06 21:28:39 -0400

    cs224 assignment 1

---
+++ nlp/cs224/assignment1/q2_neural.py
@@ -0,0 +1,88 @@
+import numpy as np
+import random
+import pdb
+
+from q1_softmax import softmax
+from q2_sigmoid import sigmoid, sigmoid_grad
+from q2_gradcheck import gradcheck_naive
+
+def forward_backward_prop(data, labels, params, dimensions):
+    """ 
+    Forward and backward propagation for a two-layer sigmoidal network 
+    
+    Compute the forward propagation and for the cross entropy cost,
+    and backward propagation for the gradients for all parameters.
+    """
+
+    ### Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
+    #pdb.set_trace()
+    W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    h1 = np.dot(data, W1)+b1
+    a1 = sigmoid(h1)
+    scores = np.dot(a1, W2)+b2
+    probs = softmax(scores)
+    
+    cost = np.sum(-scores*labels+labels*np.log(np.sum(np.exp(scores), axis=1, keepdims=True)))
+
+    #gradscores = -labels+np.exp(scores)/np.sum(np.exp(scores), axis=1, keepdims=True)
+    gradscores = -labels+probs
+
+    gradb2 = np.sum(gradscores, axis=0)
+    gradW2 = np.dot(a1.T,gradscores)
+    grada1 = np.dot(gradscores,W2.T)
+
+    gradh1 = grada1*sigmoid_grad(a1)
+    gradb1 = np.sum(gradh1, axis=0)
+    gradW1 = np.dot(data.T,gradh1)
+    
+    ### Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(), 
+        gradW2.flatten(), gradb2.flatten()))
+    
+    return cost, grad
+
+def sanity_check():
+    """
+    Set up fake data and parameters for the neural network, and test using 
+    gradcheck.
+    """
+    print "Running sanity check..."
+
+    N = 20
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i,random.randint(0,dimensions[2]-1)] = 1
+    
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+
+    gradcheck_naive(lambda params: forward_backward_prop(data, labels, params,
+        dimensions), params)
+
+def your_sanity_checks(): 
+    """
+    Use this space add any additional sanity checks by running:
+        python q2_neural.py 
+    This function will not be called by the autograder, nor will
+    your additional tests be graded.
+    """
+    print "Running your sanity checks..."
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+
+if __name__ == "__main__":
+    sanity_check()
+    your_sanity_checks()
